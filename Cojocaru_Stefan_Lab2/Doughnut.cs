d53104bef1b0d28ffd11bf2064d76fa2de3ccc14
Stefan Cojocaru <stefancojocaru313@gmail.com>
2020-11-13 15:39:52 +0200

    Laborator 3

---
+++ Cojocaru_Stefan_Lab2/Doughnut.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cojocaru_Stefan_Lab2
+{
+
+    class Doughnut
+    {
+
+        private DoughnutType mFlavor;
+
+        public DoughnutType Flavor
+        {
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+        private float mPrice;
+        public float Price
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+        public Doughnut(DoughnutType aFlavor) // constructor
+        {
+            mFlavor = aFlavor;
+        }
+
+    }
+    public enum DoughnutType
+    {
+        Glazed,
+        Sugar,
+        Lemon,
+        Chocolate,
+        Vanilla
+    }
+
+}
